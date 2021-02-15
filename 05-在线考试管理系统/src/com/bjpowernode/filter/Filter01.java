d05d200e26d2e4fb4ed4f7a121b85c4d32390ed4
dlkyy <474464152@qq.com>
2021-02-15 11:29:09 +0800

    Servlet-过滤器防止恶意登录

--- 05-在线考试管理系统/src/com/bjpowernode/filter/Filter01.java
+++ 05-在线考试管理系统/src/com/bjpowernode/filter/Filter01.java
@@ -12,14 +12,21 @@ public class Filter01 implements Filter {
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
-    //1.拦截后,通过请求对象向Tomcat索要当前用户的HttpSession
-    HttpSession session = request.getSession(false);
-    //2.判断来访用户身份合法性
-    if(session == null){
-      request.getRequestDispatcher("/login_error.html").forward(servletRequest, servletResponse);
+
+    //1.调用请求对象读取请求包中请求行中的URI，了解用户访问的资源文件
+    String uri = request.getRequestURI();
+    //2.如果本次请求的资源文件与登录相关
+    if(uri.indexOf("login") != -1 || "/myWeb/".equals(uri)){
+      filterChain.doFilter(servletRequest, servletResponse);// 放行
       return;
     }
-    //3.放行
-    filterChain.doFilter(servletRequest, servletResponse);
+    //3.如果本次访问的请求资源是其他资源文件时，需要得到用户的HttpSession
+    HttpSession session = request.getSession(false);
+    if(session != null){
+      filterChain.doFilter(servletRequest, servletResponse);// 放行
+      return;
+    }
+    // 4.拒绝请求
+    request.getRequestDispatcher("/login_error.html").forward(servletRequest, servletResponse);
   }
 }
